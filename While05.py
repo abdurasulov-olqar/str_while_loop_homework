fcc37970b91040ea789b923bfe2e48c20f8887cf
olqarabdurasulov <abdurasulovolqar123@mail.com>
2022-12-25 10:24:23 +0500

    lower letters

--- While05.py
+++ While05.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    lowerLetters = 0
+
+    while len(s)>i:
+        lowerLetters += s[i].islower()
+        i += 1
+    return lowerLetters
